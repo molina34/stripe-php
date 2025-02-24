51537e6903b2920e1fa50ff54cb70fbd7e7447fe
Stripe OpenAPI <105521251+stripe-openapi[bot]@users.noreply.github.com>
2025-02-24 17:09:39 +0000

    Update generated code for v1505

--- lib/Service/TestHelpers/Terminal/ReaderService.php
+++ lib/Service/TestHelpers/Terminal/ReaderService.php
@@ -43,4 +43,20 @@ class ReaderService extends \Stripe\Service\AbstractService
     {
         return $this->request('post', $this->buildPath('/v1/test_helpers/terminal/readers/%s/succeed_input_collection', $id), $params, $opts);
     }
+
+    /**
+     * Completes an input collection with a timeout error on a simulated reader.
+     *
+     * @param string $id
+     * @param null|array{expand?: string[]} $params
+     * @param null|RequestOptionsArray|\Stripe\Util\RequestOptions $opts
+     *
+     * @throws \Stripe\Exception\ApiErrorException if the request fails
+     *
+     * @return \Stripe\Terminal\Reader
+     */
+    public function timeoutInputCollection($id, $params = null, $opts = null)
+    {
+        return $this->request('post', $this->buildPath('/v1/test_helpers/terminal/readers/%s/timeout_input_collection', $id), $params, $opts);
+    }
 }
